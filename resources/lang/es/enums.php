24d097f83bf3903f65640d4644e8fa37bb505f19
Jesús Vielma <jesusvielma309@gmail.com>
2017-07-26 11:44:15 -0400

    Agregando traducciones de laravel

---
+++ resources/lang/es/enums.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Traduccion para los enums
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'usuario' => [
+		'th' => 'Talento Humano',
+		'admin' => 'Administrador',
+		'coordinador' => 'Coordinador',
+		'gerente' => 'Gerente',
+		'supervisor' => 'Supervisor',
+		'jefe'	=> 'Jefe'
+	]
+
+];
