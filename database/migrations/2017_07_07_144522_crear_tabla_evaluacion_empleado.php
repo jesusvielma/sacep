78e570fc98315c62a853f703110be83b2547488d
Jesús Vielma <jesusvielma309@gmail.com>
2017-08-10 15:18:22 -0400

    Haciendo tipo en evaluacion_empleado primary tambien

--- database/migrations/2017_07_07_144522_crear_tabla_evaluacion_empleado.php
+++ database/migrations/2017_07_07_144522_crear_tabla_evaluacion_empleado.php
@@ -16,8 +16,8 @@ class CrearTablaEvaluacionEmpleado extends Migration
         Schema::create('evaluacion_empleado', function (Blueprint $table) {
             $table->integer('id_evaluacion')->unsigned();
             $table->integer('cedula_empleado');
-            $table->primary(['id_evaluacion','cedula_empleado']);
             $table->enum('tipo',['coordinador','supervisor','jefe','gerente','trabajador','evaluador','th']);
+            $table->primary(['id_evaluacion','cedula_empleado','tipo']);
             $table->foreign('id_evaluacion')->references('id_evaluacion')->on('evaluacion')
             ->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('cedula_empleado')->references('cedula_empleado')->on('empleado')
